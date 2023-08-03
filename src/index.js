a58714aef0fcd3b0b62827b95425b5e5fbf3e528
herinyhert <herinyhert@gmail.com>
2023-08-03 00:39:51 -0300

    Commit cambios en el submódulo

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+import axios from 'axios';
+axios.defaults.baseURL = 'http://localhost:3010'; // Reemplaza con la URL del servidor backend
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
